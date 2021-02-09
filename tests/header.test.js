509e47155998e6853bf744950a38820714c2553d
Habiburrahman Dehzad <rhabiblv@gmail.com>
2021-02-09 20:40:01 +0430

    Fixed jest not exiting on time

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,6 +2,16 @@ const Page = require('./helpers/page');
 
 let page;
 
+beforeAll((done) => {
+  done();
+});
+
+afterAll((done) => {
+  // Closing the DB connection allows Jest to exit successfully.
+  mongoose.connection.close();
+  done();
+});
+
 beforeEach(async () => {
   page = await Page.build();
   await page.goto('http://localhost:3000');
